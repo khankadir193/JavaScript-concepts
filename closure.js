cd82a3bba00e06d7d71c69589005557b88daaee8
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-03-19 09:54:42 +0530

    closure advance topic for js

---
+++ closure.js
@@ -0,0 +1,29 @@
+// function createCaffeineAddict(coffeeType){
+//     const additction = `addicted to ${coffeeType}`;
+//     console.log('outer function..',additction);
+//     return function getHigh(){
+//         const highMessage = `Feeling weired and ${additction}`;
+//         console.log(highMessage);
+//     };
+// };
+// const latteLover = createCaffeineAddict('Latte');
+// const expressoLover = createCaffeineAddict('expresso');
+// latteLover();
+// expressoLover();
+
+//arrow function
+const createCaffeineAddict = (coffeeType) => {
+    const addiction = `addicted to ${coffeeType}`;
+    console.log('outer function..', addiction);
+    return () => {
+        const highMessage = `Feeling weird and ${addiction}`;
+        console.log(highMessage);
+    };
+};
+
+const latteLover = createCaffeineAddict('Latte');
+const espressoLover = createCaffeineAddict('espresso');
+latteLover();
+espressoLover();
+
+
