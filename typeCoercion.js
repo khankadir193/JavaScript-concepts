baaaa51150a6b562dd7f45cc423a6d4d64398691
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-03-21 11:53:11 +0530

    type coercion

---
+++ typeCoercion.js
@@ -0,0 +1,7 @@
+let a = 10;
+let b = '32';
+console.log(a + b); //it would be 1032 because concatinating not adding the value.
+
+console.log(10 * '4');
+console.log(false == 0);
+console.log(null == undefined);
