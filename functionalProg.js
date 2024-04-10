79a61beb16cfcf7e6aaad10c889986d3bc91aec8
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-04-10 15:33:47 +0530

    generator and hoisting in javascript

--- functionalProg.js
+++ functionalProg.js
@@ -31,7 +31,8 @@ const arr2 = [...arr1,4];
 console.log('arr222...',arr2);
 
 //coffee generator
-
+//generator is a function that we can pause and resume the function multiple times.
+//when generator function is called is returns generator object.
 function* coffeeGenerator(){
     const coffeeTypes = ['latte', 'cappuccino', 'espresso', 'americano'];
     for(let i=0;i<coffeeTypes.length;i++){
@@ -46,3 +47,16 @@ console.log('coffee...?',coffee.next());
 console.log('coffee...?',coffee.next());
 console.log('coffee...?',coffee.next());
 
+//javascript hoisting
+// javascript hoisting is a unexpected behavior it's moving the top of the scope
+// we accessing a variable before declaring it.var is only hoisting.
+//only declarations are hoisted.
+console.log('x hoisting:--',x);
+var x = 110;
+console.log('user hoisted',user);
+console.log('user hoisted',user);
+
+var user = 'Abdul Kadir Khan';
+function myFunction(){
+    console.log('Hello..World..!!!!');
+}
