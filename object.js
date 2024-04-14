01872dbb022925a621ae192932a290b2de0d99cb
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-04-14 19:17:01 +0530

    object destructing

--- object.js
+++ object.js
@@ -14,3 +14,16 @@ console.log('obj.name',obj.name);
 console.log('obj["name"]',obj['name']);
 console.log('obj.address.city',obj.address.city);
 console.log('obj["address"]["city"]',obj["address"]["city"]);
+
+//destructing element from the object
+
+const myObj = {
+    brName:"Abdul Jabir Khan",
+    brEducation:"12th",
+}
+
+const { brName,brEducation } = myObj;
+
+console.log('brName..',brName);
+console.log('brEducation..',brEducation);
+
