62199cc40533fd928c787752b054058826190709
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-06-28 20:10:06 +0530

    hoisting declaration explanation

---
+++ Hoisting.js
@@ -0,0 +1,4 @@
+//hoisting is a default behaviour of javascript.it's moving the top of the scope.
+//hoisting only variable declaration not initialization.
+// accessing var befor it's declaration.
+// hoisting keep in mind we can avoid unexpected behaviour and bugs of our code.
