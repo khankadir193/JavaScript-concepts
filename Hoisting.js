eb263a15316c871f3e3a9392ee108bddf6deb26a
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-06-28 20:44:34 +0530

    Hoisting function

--- Hoisting.js
+++ Hoisting.js
@@ -2,3 +2,17 @@
 //hoisting only variable declaration not initialization.
 // accessing var befor it's declaration.
 // hoisting keep in mind we can avoid unexpected behaviour and bugs of our code.
+
+console.log(message); //output : undefined
+var message = "The variable Has been hoisted";
+
+var message;
+console.log(message);
+message = "The variable Has been hoisted";
+
+
+//function invoking...
+message("Good morning"); //Good morning
+function message(name) {
+  console.log(name);
+}
