39a50a9b10d993744c7b362389e53396e8f18155
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-03 12:40:57 +0530

    sliceSplice method example

---
+++ sliceSpliceExample.js
@@ -0,0 +1,4 @@
+//slice method is used for extracting the elements from source to target index.
+// it will return new array.
+let arr = [10,20,30,40,50];
+console.log('sliceExample...',arr.slice(2,4));
