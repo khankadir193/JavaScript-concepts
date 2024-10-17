2eb50ae267a0e4145adb16d4a5473ef72bb44076
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-10-17 12:58:42 +0530

    currying example

---
+++ currying.js
@@ -0,0 +1,12 @@
+//currying:-currying is technique to convert a function with multiple arguments 
+// into sequence of functions with single argument.
+
+const curryAdd = (a)=>{
+    return function(b){
+        return function(c){
+            return a+b+c;
+        };
+    };
+};
+
+console.log(curryAdd(10)(2)(3)); // Output: 6
