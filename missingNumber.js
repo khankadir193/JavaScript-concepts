b858203ab46dbc3bcdf54734e4f4b27481ecff42
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-09 18:25:38 +0530

    testCases

--- missingNumber.js
+++ missingNumber.js
@@ -5,13 +5,13 @@ class Solution {
         if(arr.length === 1){
             return arr[0]+1;
         }
-        arr.sort();
+        arr.sort((a,b)=>a-b);
+        // console.log('After sorting arr.....',arr)
         // const arrs = fillArray(arr[0], arr.length);
         const arrs = [];
-        for(let i = arr[0] !== 0 ? 1 : 0;i<arr.length+1;i++){
+        for(let i = arr[0] !== 0 ? 1 : 0;i<=arr.length+1;i++){
             arrs.push(i);
         }
-
 
         console.log('arrs.....',arrs);
         for(let i=0;i<arr.length;i++){
@@ -19,6 +19,8 @@ class Solution {
                 return arrs[i];
             }
         }
+
+        return arrs[arr.length];
     }
 }
 
@@ -31,4 +33,6 @@ function fillArray(start, end) {
 const missNumber = new Solution();
 // console.log(missNumber.missingNumber([4, 0, 3, 1])); // Output: 2
 // console.log(missNumber.missingNumber([8, 2, 4, 5, 3, 7, 1]));
-console.log(missNumber.missingNumber([1])); // Output: 2
+// console.log(missNumber.missingNumber([1])); // Output: 2
+// console.log(missNumber.missingNumber([2,1])); // Output: 2
+// console.log(missNumber.missingNumber([13 ,5 ,4 ,10, 7 ,11, 1 ,9 ,12 ,8 ,2 ,6])); // Output: 10
