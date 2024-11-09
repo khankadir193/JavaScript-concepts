ea2d49932df92e3b86d924ec69b83ad0db3a5d8e
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-09 17:11:28 +0530

    return length value

--- missingNumber.js
+++ missingNumber.js
@@ -2,8 +2,11 @@ class Solution {
     // Function to find the missing number in the array.
     missingNumber(arr) {
         // code here
+        if(arr.length === 1){
+            return arr[0]+1;
+        }
         arr.sort();
-        const arrs = Array.from({length: arr.length+1}, (_, i) => i);
+        const arrs = fillArray(arr[0], arr.length);
         console.log('arrs.....',arrs);
         for(let i=0;i<arr.length;i++){
             if(arr[i]!==arrs[i]){
@@ -13,5 +16,13 @@ class Solution {
     }
 }
 
+
+function fillArray(start, end) {
+    return [...Array(end - start + 1).keys()].map(i => i + start);
+}
+
+
 const missNumber = new Solution();
-console.log(missNumber.missingNumber([4, 0, 3, 1])); // Output: 2
+// console.log(missNumber.missingNumber([4, 0, 3, 1])); // Output: 2
+// console.log(missNumber.missingNumber([8, 2, 4, 5, 3, 7, 1]));
+console.log(missNumber.missingNumber([1])); // Output: 2
