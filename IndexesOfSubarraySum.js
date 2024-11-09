89cd453b72ab382fc660abf63e1d869f4f466e8a
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-09 13:05:23 +0530

    flagValue

--- IndexesOfSubarraySum.js
+++ IndexesOfSubarraySum.js
@@ -1,7 +1,7 @@
 class Solution {
     subArraySum(arr, target) {
-        // code here
         let sum = 0;
+        let flag = false;
         for(let i=0; i<arr.length-1; i++) {
             sum = arr[i];
             for(let j=i+1; j<arr.length; j++) {
@@ -10,10 +10,15 @@ class Solution {
                 }
                 sum += arr[j];
                 if(sum === target){
+                    flag = true;
                     return [i+1, j+1];
                 }
             }
             sum = 0;
+        }
+
+        if(!flag){
+            return -1;
         }
 
     }
@@ -23,3 +28,4 @@ const obj = new Solution();
 // console.log(obj.subArraySum([1,2,3,7,5], 12));
 // console.log(obj.subArraySum([1,2,3,4,5,6,7,8,9,10], 15));
 // console.log(obj.subArraySum([7,2,1], 2));
+console.log(obj.subArraySum([5,3,4], 2));
