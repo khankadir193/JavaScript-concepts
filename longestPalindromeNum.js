8746ad769501225877bdb4f815451c4fae79d4d7
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-10 19:14:09 +0530

    longest plaindrome

---
+++ longestPalindromeNum.js
@@ -0,0 +1,11 @@
+class Solution {
+  longestPalin(S){
+    //code here
+  }
+};
+
+const longPalindrome = new Solution();
+longPalindrome.longestPalin("aaaabbaa"); // Output: "aaaabbaa"
+
+// problem link
+// https://www.geeksforgeeks.org/problems/longest-palindrome-in-a-string3411/1?page=1&category=Strings&sortBy=submissions
