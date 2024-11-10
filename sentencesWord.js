d1c26a40b3601216ecde612b2362e431fe760ee2
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-10 20:20:54 +0530

    sentencesWord

---
+++ sentencesWord.js
@@ -0,0 +1,15 @@
+class Solution {
+    wordSentencesCount = (txt) => {
+        if(txt.length === 1) return '1 1';
+
+        console.log('word length...',txt.length);
+
+    };
+}
+
+const obje = new Solution();
+const text = 'Sentences';
+console.log(obje.wordSentencesCount(text));
+
+// problem link
+// https://www.geeksforgeeks.org/problems/counting-number-of-sentences-and-words-in-text3226/1?page=4&category=Strings&sortBy=latest
