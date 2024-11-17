8d9b6885acb5d529bca601f68a93465b500636bf
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-17 12:41:00 +0530

    throttling function

---
+++ throttling.js
@@ -0,0 +1,5 @@
+//throttling is a technique to limit the rate of the function execution.
+//it is prevent the function from being called for each event trigger.
+//it is used to performance optimization.
+// basically throttling is useful for scrolling,typing, page resizing, keypress etc.
+
