2ecc9323012b62347b954512a613f8c9282da10a
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-20 14:21:41 +0530

    promise example

---
+++ PromiseExample2222.js
@@ -0,0 +1,18 @@
+const myPromise = new Promise((resolve, reject)=>{
+    try{
+        if(resolve) {
+            resolve('Promise resolved successfully');
+        }else if(reject) {
+            reject('Promise rejected');
+        }else{
+            throw new Error('Promise neither resolved nor rejected');
+        }
+    }catch(err){
+        console.error(err);
+    }
+});
+
+
+
+myPromise.then((result)=> console.log('result...Passed~~~',result))
+.catch((err)=> console.error('error....Failed!!!',err));
