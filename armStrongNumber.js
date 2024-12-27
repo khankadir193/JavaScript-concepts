67cc083e521d24d67e77439e4879028efc896199
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-12-27 20:45:32 +0530

    armStrong

---
+++ armStrongNumber.js
@@ -0,0 +1,19 @@
+class Solution {
+    armstrongNumber(n) {
+        // code here
+        let strNum = String(n);
+        let totalVal = 0;
+        for(let numChar of strNum){
+            const num = Number(numChar);
+            totalVal += num * num * num;
+        }
+
+        if(totalVal === n) return true;
+
+        return false;
+    }
+}
+
+const n = 153;
+const newObjRef = new Solution(n);
+console.log(newObjRef.armstrongNumber(n));
