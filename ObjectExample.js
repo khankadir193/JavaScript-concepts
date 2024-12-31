d4097194f819010f80716e0dd86ba67d9876e601
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-12-31 21:44:25 +0530

    objectExample

--- ObjectExample.js
+++ ObjectExample.js
@@ -18,3 +18,14 @@ const obj2 = {
 Object.assign(obj,obj2); // it is used for copying object(target,source);
 console.log('obj2222',obj);
 
+//freez method is used for we can modify the object
+Object.freeze(obj2);
+obj2.name = 'khan';
+console.log('afterFreezobj',obj2);
+
+//seal method:- this method is used for preventing add/remove but allow modification
+Object.seal(obj);
+obj['addProperty'] = 'newProperty'; // this will not work
+obj['name'] = 'Rehan khan'; // it will work accordingly...
+console.log('afterSealMethod...',obj);
+
