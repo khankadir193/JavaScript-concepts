6b902d850dc6ab41bc171a63370231e2a66bfe8b
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-12-31 22:27:21 +0530

    implementAtoi

---
+++ implementAtoi.js
@@ -0,0 +1,10 @@
+class Solution {
+    myAtoi(s) {
+        // console.log('s...',Number(s));
+        console.log('s...',s);
+    }
+};
+
+const obje = new Solution();
+const str = '-123';
+obje.myAtoi(str);
