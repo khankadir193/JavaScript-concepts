51cded8abd3850aa901356907fbf2a3edeab1453
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2025-01-01 17:01:40 +0530

    memoization after refactored

--- memoizationExample.js
+++ memoizationExample.js
@@ -1,23 +1,31 @@
 const memoizationTechnique = () => {
-    const temp = {};
-    return (props) => {
-        if (props in temp) {
-            console.log('props...',temp);
-            return temp[props];
+    const cache = {};
+    return (input) => {
+        console.time(`Time taken for input: ${input}`);
+        if (input in cache) {
+            console.log('Cache hit:', cache);
+            console.timeEnd(`Time taken for input: ${input}`);
+            return cache[input];
         } else {
-            let sum = props + 43;
-            return temp[props] = sum;
+            const result = input + 43;
+            cache[input] = result;
+            console.log('Cache miss:', cache);
+            console.timeEnd(`Time taken for input: ${input}`);
+            return result;
         }
     };
 };
 
 const memoized = memoizationTechnique();
 
-console.time();
-console.log(memoized(34));
-console.timeEnd();
+console.log('First call with input 34:');
+console.log('Result:', memoized(34));
 
-console.log('again calling same input');
-console.time();
-console.log(memoized(23));
-console.timeEnd();
+console.log('\nCalling with input 34:');
+console.log('Result:', memoized(34));
+
+// console.log('\nCalling again with input 34:');
+// console.log('Result:', memoized(34));
+
+// console.log('\nCalling again with input 23:');
+// console.log('Result:', memoized(23));
