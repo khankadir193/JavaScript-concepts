c77a2a034791f7199557efcbd44597261196a1f4
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2025-01-05 11:00:00 +0530

    lowerBound has been resolved

--- lowerBound.js
+++ lowerBound.js
@@ -4,19 +4,18 @@ class Solution {
     lowerBound(arr, target) {
         // your code here
         const eleArr = arr.filter((item)=>{
-            if(item>target){
+            if(item>=target){
                 return item;
             }
         });
-        console.log('eleArr....',eleArr);
-        // console.log('arr...',arr,'target...',target);
 
         const mathArr = Math.min(...eleArr);
-        console.log('mathArr....',mathArr);
         console.log('findIndex...',arr.indexOf(mathArr))
     }
 };
 
 const solution = new Solution();
-const arr = [2, 3, 7, 10, 11, 11, 25], target = 9;
+// const arr = [2, 3, 7, 10, 11, 11, 25], target = 9;
+const arr = [2, 3, 7, 10, 11, 11, 25], target = 11;
+
 solution.lowerBound(arr,target);
