262d3f7907457527d341ff1a88cf2ca1834caba3
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2025-01-05 15:00:30 +0530

    squareRoot

---
+++ SquareRoot.js
@@ -0,0 +1,11 @@
+class Solution {
+
+    floorSqrt(n) {
+        // your code here
+        console.log('square...',Math.floor(Math.sqrt(n)));
+    }
+}
+
+const sqrt = new Solution();
+const n = 11;
+sqrt.floorSqrt(n);
