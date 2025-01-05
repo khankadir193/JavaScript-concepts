90e18d49b1dfd42890aab1d18f1fcf256d6059a9
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2025-01-05 10:54:48 +0530

    lowerBound

---
+++ lowerBound.js
@@ -0,0 +1,21 @@
+// https://www.geeksforgeeks.org/batch/gfg-160-problems/track/searching-bonus-problems/problem/implement-lower-bound
+
+class Solution {
+    lowerBound(arr, target) {
+        // your code here
+        const eleArr = arr.filter((item)=>{
+            if(item>target){
+                return item;
+            }
+        });
+        console.log('eleArr....',eleArr);
+        // console.log('arr...',arr,'target...',target);
+
+        const mathArr = Math.min(...eleArr);
+        console.log('mathArr....',mathArr);
+    }
+};
+
+const solution = new Solution();
+const arr = [2, 3, 7, 10, 11, 11, 25], target = 9;
+solution.lowerBound(arr,target);
