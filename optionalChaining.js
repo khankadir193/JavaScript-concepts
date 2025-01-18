cdbd1bef9f4cffa5da621295a515f06235ae3219
= <=>
2025-01-18 22:44:28 +0530

    optionalChaining

--- optionalChaining.js
+++ optionalChaining.js
@@ -1,7 +1,20 @@
-const obj = {
-    name:"Abdul Kadir Khan",
-    address:"Gorakhpur, Uttarpradesh",
+// const obj = {
+//     name:"Abdul Kadir Khan",
+//     address:"Gorakhpur, Uttarpradesh",
 
-};
+// };
 
-console.log(obj.surName);
+// console.log(obj.surName);
+
+//The optional chaining operator ?. allows you to safely access deeply nested object properties 
+// without worrying about encountering undefined or null errors.
+
+const user = { name: "Alice", address: { city: "Wonderland" } };
+
+console.log(user?.address?.city); // "Wonderland"
+console.log(user?.contact?.phone); // undefined (does not throw an error)
+
+
+//nullish operator :- this operator check only null/undefined operator.
+
+// const  
