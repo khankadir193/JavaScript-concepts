44ba2c47d2b6fb0426adbd698fe9b913ee10d99f
= <=>
2025-01-19 20:12:05 +0530

    number of occurence

---
+++ eleminatingNumber.js
@@ -0,0 +1,12 @@
+const eliminatingNumber = (arr)=>{
+    for(let i=0;i<arr.length;i++){
+        
+    }
+};
+
+
+const arr = [21,34,56,78,12];
+console.log('invocation function',eliminatingNumber(arr));
+
+//url
+//https://www.geeksforgeeks.org/problems/remaining-string3515/1?page=4&category=Strings&sortBy=submissions
