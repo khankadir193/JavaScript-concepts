88d6424b0cc05bd6de68b4f7d41aadc0354085e9
= <=>
2025-01-19 15:34:22 +0530

    short-regular function declaration

---
+++ functionType.js
@@ -0,0 +1,8 @@
+//short and concise way..
+const add = (a,b)=> a+b;
+console.log('short-concise',add(10,23));
+//regular function
+function addReg(a,b){
+    return a+b;
+};
+console.log('regular-function',addReg(32,23));
