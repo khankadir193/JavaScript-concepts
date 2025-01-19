487c4dcbce918f18c5b651f83fb0006c3334b348
= <=>
2025-01-19 15:38:56 +0530

    functional types

--- functionType.js
+++ functionType.js
@@ -1,7 +1,11 @@
 //short and concise way..
+//does not have arguments object 
+//it's not pointing this object
 const add = (a,b)=> a+b;
 console.log('short-concise',add(10,23));
 //regular function
+//it's having own argument objects
+//it's pointing this objects
 function addReg(a,b){
     return a+b;
 };
