b7174d44ead843918e50e165c84dd2132ae2c25a
Abdul Kadir Khan <abdul.khan@fugetroncorp.com>
2025-02-22 19:02:16 +0530

    remainingString

--- remainingString.js
+++ remainingString.js
@@ -5,8 +5,9 @@ const eliminatingNumber = (arr)=>{
 };
 
 
-const arr = [21,34,56,78,12];
-console.log('invocation function',eliminatingNumber(arr));
+// const arr = [21,34,56,78,12];
+const str = 'Thisisdemostring'
+console.log('invocation function',eliminatingNumber(str));
 
 //url
 //https://www.geeksforgeeks.org/problems/remaining-string3515/1?page=4&category=Strings&sortBy=submissions
