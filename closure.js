daeb7cf4e43c9cbced8ef3e2961b4dd22e709f21
Abdul Kadir Khan <abdul.khan@fugetroncorp.com>
2025-04-08 12:22:10 +0530

    closure example

--- closure.js
+++ closure.js
@@ -1,33 +1,35 @@
-// function createCaffeineAddict(coffeeType){
-//     const additction = `addicted to ${coffeeType}`;
-//     console.log('outer function..',additction);
-//     return function getHigh(){
-//         const highMessage = `Feeling weired and ${additction}`;
-//         console.log(highMessage);
-//     };
-// };
-// const latteLover = createCaffeineAddict('Latte');
-// const expressoLover = createCaffeineAddict('expresso');
-// latteLover();
-// expressoLover();
-
-//arrow function
-const createCaffeineAddict = (coffeeType) => {
-    const addiction = `addicted to ${coffeeType}`;
-    console.log('outer function..', addiction);
-    return () => {
-        const highMessage = `Feeling weird and ${addiction}`;
+// closure example into the 
+function createCaffeineAddict(coffeeType){
+    const additction = `addicted to ${coffeeType}`;
+    console.log('outer function..',additction);
+    return function getHigh(){
+        const highMessage = `Feeling weired and ${additction}`;
         console.log(highMessage);
     };
 };
-
 const latteLover = createCaffeineAddict('Latte');
-const espressoLover = createCaffeineAddict('espresso');
+const expressoLover = createCaffeineAddict('expresso');
 latteLover();
-espressoLover();
+expressoLover();
+
+//arrow function
+// const createCaffeineAddict = (coffeeType) => {
+//     const addiction = `addicted to ${coffeeType}`;
+//     console.log('outer function..', addiction);
+//     return () => {
+//         const highMessage = `Feeling weird and ${addiction}`;
+//         console.log(highMessage);
+//     };
+// };
+
+// const latteLover = createCaffeineAddict('Latte');
+// const espressoLover = createCaffeineAddict('espresso');
+// latteLover();
+// espressoLover();
 
 
 //another approach  for closure..
+console.log('--------another approach for clousre----------');
 const  greet = (name) =>{
     return ()=>{
         console.log(`Hi ${name}`);     
