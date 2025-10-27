f2c17b44948f6b206fe0acfe10f3c34f71079df1
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2025-10-27 17:01:09 +0530

    Pangram code

---
+++ Pangram.js
@@ -0,0 +1,37 @@
+// https://www.geeksforgeeks.org/dsa/pangram-checking/
+
+function Pangram(str){
+    let smallLetter = str.toLowerCase();
+    let strArr = smallLetter.split('');
+    strArr.sort();
+    let strSort = strArr.join('').trim();
+
+    // for(let i=97; i<=122; i++){
+    //     if()
+    // }
+
+    const set = new Set(strSort);
+
+    const unDuplicateArr = Array.from(set);
+
+    console.log('set.....',unDuplicateArr);
+
+    if(unDuplicateArr.length < 26) return false;
+
+    let counter = 97;
+    let flag = true;
+    unDuplicateArr.forEach((char)=>{
+        if(char.charCodeAt(0) !== counter){
+            flag = false;
+            return false;
+        };
+        counter++;
+    });
+
+    return flag;
+};
+
+// let str = 'The quick brown fox jumps over the lazy dog';
+let str = 'The quick brown fox jumps over the dog';
+
+console.log('Pangram is',Pangram(str));
