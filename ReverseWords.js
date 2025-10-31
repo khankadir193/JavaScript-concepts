72b2b2b4b56f253decfbe5574d3c77b5973a3e23
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2025-10-31 19:13:46 +0530

    reverse words

--- ReverseWords.js
+++ ReverseWords.js
@@ -1,3 +1,4 @@
+// https://www.geeksforgeeks.org/dsa/reverse-words-in-a-given-string/
 function ReverseWords(str) {
     let flag = false;
     let arrSplit = str.split('.');
@@ -10,17 +11,23 @@ function ReverseWords(str) {
     if (flag === false) {
         // console.log('....',arrSplit.reverse());
         arrSplit.reverse();
-        // arrSplit.forEach((item)=>{
-        //     // tempStr.concat(item);
-        //     tempStr += item + '.';
-        // });
-        for(ele of arrSplit){
-            tempStr += ele + '.';
-        }
+        arrSplit.forEach((item, index) => {
+            if (arrSplit.length - 1 === index) {
+                tempStr += item;
+            } else {
+                tempStr += item + '.';
+            }
+        });
+    } else {
+        arrSplit.reverse();
+        arrSplit.forEach((item, index) => {
+            tempStr += item + ' ';
+        });
     }
 
     console.log('tempStr....', tempStr);
 };
-const str = 'i.like.this.program.very.much';
+// const str = 'i.like.this.program.very.much';
 // const str = 'i like this program very much';
+const str = '..geeks..for.geeks.' ;
 console.log('ReverseWord', ReverseWords(str));
