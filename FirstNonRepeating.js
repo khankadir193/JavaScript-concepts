6b35566d376c77ede0ec8c08d9dc31a6c7a87837
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2025-10-31 20:24:58 +0530

    firstNonRepeating character

---
+++ FirstNonRepeating.js
@@ -0,0 +1,20 @@
+// https://www.geeksforgeeks.org/dsa/given-a-string-find-its-first-non-repeating-character/
+
+function firstNonRepeatingChar(str){
+    const frequency = {};
+    for(const ele of str){
+        frequency[ele] = (frequency[ele] || 0) + 1;
+    };
+
+    for(let [key,value] of Object.entries(frequency)){
+        if(value === 1){
+            return key;
+        }
+    }
+
+    return '$';
+};
+// const str = 'geeksforgeeks';
+// const str = 'racecar';
+const str = 'aabbccc';
+console.log('first non repeating char',firstNonRepeatingChar(str));
