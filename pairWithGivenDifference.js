2a40f8e72f8d8e36d7a7d7cde02fec82a5efa3ba
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2025-11-10 17:15:57 +0530

    pairwithdifference

---
+++ pairWithGivenDifference.js
@@ -0,0 +1,13 @@
+function PairWithDifference(arr,x){
+    for(let i=0;i<arr.length-1;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if(Math.abs(arr[j] - arr[i]) === x) return 'Yes';
+        }
+    };
+
+    return 'No';
+};
+
+// const arr = [5, 20, 3, 2, 50, 80], x = 78;
+const arr = [90, 70, 20, 80, 50], x = 45;
+console.log('PairWithDifference...',PairWithDifference(arr,x));
