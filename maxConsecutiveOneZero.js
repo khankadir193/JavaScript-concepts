4a13afa5557d178ef982c35637b1026b8dad70e5
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2025-11-12 18:15:07 +0530

    max consecutive zero

---
+++ maxConsecutiveOneZero.js
@@ -0,0 +1,24 @@
+function maximumConsecutiveOne(arr) {
+    let tempOne = 0, tempZero = 0;
+    const tempArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            tempArr.push(tempOne);
+            tempOne = 0;
+            tempZero++;
+        } else if (arr[i] === 1) {
+            tempArr.push(tempZero);
+            tempZero = 0;
+            tempOne++;
+        }
+    }
+
+    if (tempOne > tempZero) tempArr.push(tempOne);
+    else tempArr.push(tempZero);
+    return Math.max(...tempArr);
+}
+
+const arr = [0, 0, 0, 1, 0, 1, 1, 1];
+// const arr = [0, 0, 1, 0, 1, 0];
+// const arr = [0, 0, 0, 0];
+console.log('maximumConsecutive...?', maximumConsecutiveOne(arr));
