fe0526d5aa768616e182804e19a6bfd563f00b34
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2025-11-12 16:45:40 +0530

    pairWithDifference

--- pairWithGivenDifference.js
+++ pairWithGivenDifference.js
@@ -1,3 +1,4 @@
+// https://www.geeksforgeeks.org/dsa/find-a-pair-with-the-given-difference/
 function PairWithDifference(arr,x){
     for(let i=0;i<arr.length-1;i++){
         for(let j=i+1;j<arr.length;j++){
