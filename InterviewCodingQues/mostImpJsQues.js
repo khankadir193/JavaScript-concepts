9bf1058383204d279245edf560cee59206815db7
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2026-02-17 16:44:26 +0530

    most imp ques

--- InterviewCodingQues/mostImpJsQues.js
+++ InterviewCodingQues/mostImpJsQues.js
@@ -120,3 +120,122 @@ console.log(false == ![]); // here [] is truthy value directly convert into fals
 console.log([] != []); // arrays are objects it's compared by reference not value.
 console.log([1] == [1]); // arrays are objects compared by reference. === does NOT do type coercion Objects are still compared by reference
 console.log({} != {}); // Arrays and objects are compared by reference
+
+//count the word
+console.log('count the word');
+function countWord(str){
+    //first approach along with inbuilt method..
+    // let splitArr = str.split(' ').filter(item => item.trim());
+    // console.log(splitArr.length);
+    
+    //second logical approach..
+    // const splArr = str.split(' ');
+    // let count = 0;
+    // for(let word of splArr){
+    //     if(word !== ''){
+    //         count++
+    //         console.log(word)
+    //     }
+    // }
+
+    // return count;
+
+    // third pure logic approach
+    let count = 0;
+    let isWord = false;
+    for(let i=0;i<str.length;i++){
+        if(str[i] !== ' ' && !isWord){
+            count++;
+            isWord = true;
+        }else if(str[i] === ' '){
+            isWord = false;
+        }
+    }
+
+    return count;
+    
+    // console.log('---',count);
+};
+
+let str = 'code    step   by       step        khan';
+console.log('countword----',countWord(str));
+
+//missing Number 
+console.log('missing number');
+
+function missingNum(arr){
+    //first approach
+    // let count = 1;
+    // for(let i=0;i<arr.length;i++){
+    //     if(arr[i] === count){
+    //         count++;
+    //     }else{
+    //         return count;
+    //     }
+    // }
+
+    //second approach
+    let n = 5;
+
+    let total = n*(n+1)/2;
+    let totalSum = arr.reduce((pre,curr) => pre + curr,0);
+    let missNum = total - totalSum;
+
+    return missNum;
+};
+
+
+const missNum = [1,2,3,5];
+
+console.log('missNumber',missingNum(missNum));
+
+console.log('Empty Items...');
+function removeEmptyItems(arr){
+    //first approach with inbuilt method
+    // const filterEle = arr.filter(item => item);
+    // console.log(filterEle);
+    
+    //second approach logical approach    
+    const filterEle2 = arr.filter(Boolean);
+    console.log('filterEle222',filterEle2);
+};
+
+const emptyArr = [20,4,6,,1,5,1,6,2,,,,7,3];
+console.log('--',removeEmptyItems(emptyArr));
+
+// using object property using short cut
+console.log('using object along with short cut...');
+const user = {
+    name:'Kadir',
+    address:{
+        city:'Gorakhpur'
+    }
+};
+
+// if(user && user.address && user.address.city){
+//     console.log(user.address.city);
+// }else{
+//     console.log('something went wrong.');
+// }
+
+// or
+console.log(user.address?.city && user.address.city);
+console.log(user.address?.city ?? 'something went wrong');
+
+console.log('convert capital letter into small letter');
+function convertInSmall(info){
+    let string = '';
+    for(let i=0;i<info.length;i++){
+        let code = info.charCodeAt(i);
+        if(code<=90 && code >= 65){
+            string += String.fromCharCode(code+32);
+        }else{
+            string += info[i];
+        }
+    };
+    
+    console.log('smallLetters...',string)
+};
+// let info = 'heLLo How aRe You.';
+let info = 'Abdul KADIR khaN'
+convertInSmall(info);
