f13fc5dfd34ddbd491082a76d45c589d50caa016
khankadir193 <abdulkadirk059@gmail.com>
2026-05-25 17:04:10 +0530

    set imp

--- last_Round_JS_Practice/removeDuplicates.js
+++ last_Round_JS_Practice/removeDuplicates.js
@@ -14,16 +14,21 @@ function removeDuplicates(arr){
     // return tempArr;
 
     //second most better approach
-    const seen = {};
-    const seenArr = [];
-    for(let element of arr){
-        if(!seen[element]){
-            seen[element] = true;
-            seenArr.push(element);
-        }
-    }
+    // const seen = {};
+    // const seenArr = [];
+    // for(let element of arr){
+    //     if(!seen[element]){
+    //         seen[element] = true;
+    //         seenArr.push(element);
+    //     }
+    // }
 
-    return seenArr;
+    // return seenArr;
+    
+    // third approach for set object.
+    const set = new Set(arr); // set is a object which store the collections of elements in unique.
+    console.log('set...values',Array.from(set)); // array.from is a static method which is 
+    // creates the new shallow copied array instance from an iterable or array like object.
 };
 
 const arr = [1, 2, 2, 3, 4, 4, 5];
