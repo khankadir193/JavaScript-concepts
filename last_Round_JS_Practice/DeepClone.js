b3fe424e4c1100daff89f378ba449447d3160af8
khankadir193 <abdulkadirk059@gmail.com>
2026-05-25 17:55:38 +0530

    deep clone

---
+++ last_Round_JS_Practice/DeepClone.js
@@ -0,0 +1,26 @@
+const user = {
+    name:'Abdul Kadir Khan',
+    address:{
+        city:'Gorakhpur',
+        zipCode:{
+            zidAdd:272151
+        }
+    }
+}
+
+const copy = {...user};
+copy.name = 'Jabir';
+copy.address.city = 'pandri';
+console.log('original Object',user); // name will be unchanged because it's copied direct primitive value.
+console.log('copied object',copy); //  address object will be changed for both parent/child object because
+//it's copied the reference of mememory and it's pointing same location.
+
+//deep clone methods
+const clone = structuredClone(user);
+clone.address.city = 'lucknow';
+console.log('clone original obj',user);
+console.log('clone copied obj',clone);
+
+// another method is the JSON.stringify. this is the old method.
+console.log('json',JSON.stringify(user));
+
