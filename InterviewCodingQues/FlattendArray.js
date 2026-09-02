35381e9e767a0257488c2ee057f5dd206dd1dd90
khankadir193 <abdulkadirk059@gmail.com>
2026-09-02 18:21:42 +0530

    refactor flattendArray function and enhance event loop examples with additional explanations and scenarios

--- InterviewCodingQues/FlattendArray.js
+++ InterviewCodingQues/FlattendArray.js
@@ -1,20 +1,34 @@
 function flattendArray(arr) {
-    console.log('arr...???',arr);
+    console.log('arr...???', arr);
 
     // return arr.reduce((acc, val) => 
     //     Array.isArray(val) ? acc.concat(flattendArray(val)) : acc.concat(val),[]);
 
     // or
     const tempArr = [];
-    for(let element of arr){
-        if(Array.isArray(element)){
+    for (let element of arr) {
+        if (Array.isArray(element)) {
             tempArr.push(...flattendArray(element));
-        }else{
+        } else {
             tempArr.push(element);
         }
     }
 
     return tempArr;
+};
+
+
+// or
+
+const flattenArray = (arr) => {
+    return arr.reduce((result, element) => {
+        if (Array.isArray(element)) {
+            return result.concat(flattenArray(element));
+        }
+
+        result.push(element);
+        return result;
+    }, []);
 };
 
 const nestedArray = [1, [2, [3, [4, [5]]]]];
